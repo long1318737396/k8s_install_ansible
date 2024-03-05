4b0c8bcc66eaf56f01e6b186259456e8bce45d8e
long1318737396 <long1318737396@163.com>
2024-03-05 17:09:07 +0800

    update

---
+++ offline/images/arm64/push.sh
@@ -0,0 +1,24 @@
+set -x
+dir="$(cd "$(dirname "$0")" && pwd)"
+cd $dir
+source ../../../conf/config.sh
+
+
+exec > >(tee -a "$logfile") 2>&1
+echo "$date_format"
+
+
+destination=myharbor.mtywcloud.com/library
+while read line; \
+  do nerdctl tag $line ${destination}/${line##*/}; \
+     echo "----nerdctl tag $line ${destination}/${line##*/}-----"; \
+    nerdctl push --insecure-registry ${destination}/${line##*/} --platform arm64; \
+    echo "----nerdctl push --insecure-registry ${destination}/${line##*/}----"; \
+  done <base-image.list
+
+while read line; \
+  do nerdctl tag $line ${destination}/${line##*/}; \
+     echo "----nerdctl tag $line ${destination}/${line##*/}-----"; \
+    nerdctl push --insecure-registry ${destination}/${line##*/} --platform arm64; \
+    echo "----nerdctl push --insecure-registry ${destination}/${line##*/}----"; \
+  done <addon-image.list

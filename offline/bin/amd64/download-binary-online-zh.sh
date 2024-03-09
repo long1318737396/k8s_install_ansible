a53675ac7c43f84ad67940e44aa6070fe3f09113
long1318737396 <long1318737396@163.com>
2024-03-09 21:59:17 +0800

    add etcd

--- offline/bin/amd64/download-binary-online-zh.sh
+++ offline/bin/amd64/download-binary-online-zh.sh
@@ -6,7 +6,9 @@ source ../../../conf/config.sh
 exec > >(tee -a "$logfile") 2>&1
 echo "$date_format"
 
+packages_dir="../../../_output"
 
+mkdir -p ${packages_dir}/bin
 # https://github.com/containernetworking/plugins/releases/
 # https://github.com/containerd/containerd/releases/
 # https://github.com/containerd/nerdctl/releases
@@ -52,6 +54,7 @@ hubble_url="https://github.com/cilium/hubble/releases/download/${hubble_version}
 velero_url="https://github.com/vmware-tanzu/velero/releases/download/${velero_version}/velero-${velero_version}-linux-${arch}.tar.gz"
 skopeo_url="https://github.com/lework/skopeo-binary/releases/download/${skopeo_version}/skopeo-linux-${arch}"
 yq_url="https://github.com/mikefarah/yq/releases/download/${yq_version}/yq_linux_${arch}"
+jq_url="https://github.com/jqlang/jq/releases/download/jq-${jq_version}/jq-linux-${arch}"
 # curl  -k -L -C - -o minio  https://dl.min.io/server/minio/release/linux-amd64/minio
 # curl  -k -L -C - -o mc https://dl.min.io/client/mc/release/linux-amd64/mc
 packages=(
@@ -79,11 +82,12 @@ packages=(
   ${base_url}/$velero_url
   ${base_url}/$skopeo_url
   ${base_url}/$yq_url
+  ${base_url}/$jq_url
 )
 
 for package_url in "${packages[@]}"; do
   filename=$(basename "$package_url")
-  if curl  -k -L -C - -o "$filename" "$package_url"; then
+  if curl  -k -L -C - -o ${packages_dir}/bin/"$filename" "$package_url"; then
     echo "Downloaded $filename"
   else
     echo "Failed to download $filename"
--- offline/bin/amd64/download-binary-online.sh
+++ offline/bin/amd64/download-binary-online.sh
@@ -51,8 +51,9 @@ hubble_url="https://github.com/cilium/hubble/releases/download/${hubble_version}
 velero_url="https://github.com/vmware-tanzu/velero/releases/download/${velero_version}/velero-${velero_version}-linux-${arch}.tar.gz"
 skopeo_url="https://github.com/lework/skopeo-binary/releases/download/${skopeo_version}/skopeo-linux-${arch}"
 yq_url="https://github.com/mikefarah/yq/releases/download/${yq_version}/yq_linux_${arch}"
-curl  -k -L -C - -o minio  https://dl.min.io/server/minio/release/linux-amd64/minio
-curl  -k -L -C - -o mc https://dl.min.io/client/mc/release/linux-amd64/mc
+# curl  -k -L -C - -o minio  https://dl.min.io/server/minio/release/linux-amd64/minio
+# curl  -k -L -C - -o mc https://dl.min.io/client/mc/release/linux-amd64/mc
+jq_url="https://github.com/jqlang/jq/releases/download/jq-${jq_version}/jq-linux-${arch}"
 packages=(
   #$kernel_url
   #$runc_url
@@ -70,7 +71,7 @@ packages=(
   $kubernetes_server_url
   #$nginx_url
   #${cri_o_url}
-  #$harbor_url
+  $harbor_url
   $docker_compose_url
   $calicoctl_url
   $cilium_url
@@ -78,6 +79,7 @@ packages=(
   $velero_url
   $skopeo_url
   $yq_url
+  $jq_url
 )
 
 for package_url in "${packages[@]}"; do
